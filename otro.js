ac4e5380455999bbb8d49bd1f309e08b4a3b5424
Luis Mariano Lopez lena <luismllv89@gmail.com>
2023-02-28 22:21:47 -0600

    update

--- otro.js
+++ otro.js
@@ -1,2 +1,3 @@
 // db user:mern_user
 // db pass:mt7aBuykEDuTKIO2
+//mongodb://mern_user:mt7aBuykEDuTKIO2@ac-k3apbpz-shard-00-00.l6jl2nm.mongodb.net:27017,ac-k3apbpz-shard-00-01.l6jl2nm.mongodb.net:27017,ac-k3apbpz-shard-00-02.l6jl2nm.mongodb.net:27017/?ssl=true&replicaSet=atlas-3t5m1h-shard-0&authSource=admin&retryWrites=true&w=majority
